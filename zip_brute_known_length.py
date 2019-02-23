20e774709b6fdf565355e15b6d5946df0cf5387c
37bird <39220220+37bird@users.noreply.github.com>
2019-02-23 12:37:44 +0000

    made repeat object iterable

--- zip_brute_known_length.py
+++ zip_brute_known_length.py
@@ -6,7 +6,7 @@ import time
 
 # The file name of the zip file, followed by length of the passwords to try. Change as necessary.
 zip_file_name = ''
-repeat = 4
+rept = 4
 
 def extract_file(zippy, paswrd):
     try:
@@ -20,7 +20,7 @@ def extract_file(zippy, paswrd):
 alphabet = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890'
 zip_file = zipfile.ZipFile(zip_file_name)
 
-for c in itertools.product(alphabet, repeat):
+for c in itertools.product(alphabet, repeat=rept):
     # Slowing it down on purpose to make it work better with web terminals
     # Remove at your peril
     time.sleep(0.001)
